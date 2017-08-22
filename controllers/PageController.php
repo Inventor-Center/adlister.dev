0eee90544e3ff9a42e19de873ca24a05ea158484
Cody <cody@hastings.email>
2017-08-22 10:18:48 -0500

    fixed error on edit and page controller

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -70,7 +70,9 @@ function pageController()
     $allAds = Ad::all();
     $allUsersAds = User::usersAds();
     $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    if(isset($_SESSION['IS_LOGGED_IN'])){
     $currentUserInfo = User::find($_SESSION['LOGGED_IN_ID']);
+    }
    
     if(Input::has('editUsername')){
         updateUser();
